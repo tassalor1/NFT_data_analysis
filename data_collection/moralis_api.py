53d89fd4f3f07864d25951c608ae877beec0487c
tassalor1 <walkerconnor14@gmail.com>
2023-04-03 21:44:48 +0100

    moralis api

--- data_collection/moralis_api.py
+++ data_collection/moralis_api.py
@@ -1,21 +1,46 @@
 from moralis import evm_api
+import time
 
 api_key = "RaIYwygywhhv3PyS2BXs8ty85CYWY2Qys3XBFDPhdrqE8tdO1yD5CojR1Z7RYuNU"
+
+start_block = 16308190  # First block of 2023
+
+# End block params
 params = {
-    "block_number_or_hash": "15846571",
-    "chain": "eth",
-    # "limit": 100,
-    # "cursor": "",
+    "date": str(int(time.time())), # unix time
+    "chain": "eth"
 }
 
-result = evm_api.nft.get_nft_transfers_by_block(
+# End block
+result = evm_api.block.get_date_to_block(
     api_key=api_key,
     params=params,
 )
+end_block = result["block"]
+
 sales = []
-for transfer in result["result"]:
-    if int(transfer["value"]) > 0:
-        sales.append(transfer)
 
-print(sales)
+# For loop
+for block_number in range(start_block, end_block + 1):
+    params1 = {
+        "block_number_or_hash": str(block_number),
+        "chain": "eth",
+    }
 
+    result1 = evm_api.nft.get_nft_transfers_by_block(
+        api_key=api_key,
+        params=params1,
+    )
+
+    # Only show value above 0 - which is a sale
+    for transfer in result1["result"]:
+        if int(transfer["value"]) > 0:
+            sales.append(transfer)
+
+# Only print value
+# Convert wei to eth
+for sale in sales:
+    time_stamp = sale["block_timestamp"]
+    ether_value = int(sale["value"]) / 10**18
+    print(time_stamp)
+    print(ether_value)
